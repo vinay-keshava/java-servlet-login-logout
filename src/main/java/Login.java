6cf3d01c83a594091aa4d9f3d539b3903660672c
vinay-keshava <its.vicky30@gmail.com>
2022-09-05 18:17:57 +0530

    Skeleton

---
+++ src/main/java/Login.java
@@ -0,0 +1,52 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+  
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			String username=request.getParameter("name");
+			String password=request.getParameter("pass");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/loginout","root","Vinay@123");
+			String query1="select * from login where user=? and pass=?";
+			PreparedStatement pt=con.prepareStatement(query1);
+		    pt.setString(1,username);
+		    pt.setString(2,password);
+		    PrintWriter pw=response.getWriter();
+		    ResultSet rs= pt.executeQuery();
+		    if(rs.next())
+		    		pw.write("Sucesssful Loginnnn");
+		    else
+		    		pw.write("Wrong Password");
+		    	
+		}
+
+catch(SQLException e){
+	System.out.println("Could not connect to the database"+e.getMessage());
+	e.printStackTrace();
+} catch (ClassNotFoundException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}	
+	
+	}
+
+}
---
+++ src/main/java/SignUp.java
@@ -0,0 +1,85 @@
+
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/SignUp")
+public class SignUp extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			response.setContentType("text/html");
+//			
+//			String dbURL=;
+//			String USER="root";
+//			String PASS="Vinay@123";			
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				String username=request.getParameter("name");
+				String password=request.getParameter("pass");
+				Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/loginout","root","Vinay@123");
+				String query1="insert into login values(?,?)";
+				PreparedStatement pt=con.prepareStatement(query1);
+			    pt.setString(1,username);
+			    pt.setString(2,password);
+			    pt.execute();
+			}
+	
+	catch(SQLException e){
+		System.out.println("Could not connect to the database"+e.getMessage());
+		e.printStackTrace();
+	} catch (ClassNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}	
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//<servlet>
+//<servlet-name>SignUp</servlet-name>
+//	<servlet-class>SignUp</servlet-class>
+//</servlet>
+//<servlet>
+//<servlet-name>login</servlet-name>
+//	<servlet-class>Login</servlet-class>
+//</servlet>
+//
+//
+//<servlet-mapping>
+//<servlet-name>login</servlet-name>
+//<url-pattern>/login</url-pattern>
+//</servlet-mapping>
+//
+//
+//<servlet-mapping>
+//<servlet-name>SignUp</servlet-name>
+//<url-pattern>/signup</url-pattern>
+//</servlet-mapping>
+
